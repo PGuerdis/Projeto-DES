c41e87c11d2fd78e136da16479252d2a818ddd9b
PGuerdis <88003748+PGuerdis@users.noreply.github.com>
2021-11-08 13:07:34 -0300

    Add files via upload

---
+++ Projeto/Projeto/Carrinho.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto
+{
+    public class Carrinho
+    {
+        private double valortotal;
+        private double quantidadetotal;
+
+        public void VerCarrinho()
+        {
+
+        }
+    }
+}
